04b1363f5ae80b1be89bdaf3f3ea08969502e708
LaZy1220 <4elove4iska@gmail.com>
2022-05-19 10:44:11 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://movieland.mx/wp-content/uploads/2017/10/disney-logo.jpg' />
+        </header>
+    )
+}
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+const Profile = () => {
+    return (
+        <div className='profile'>
+            <img src='https://img2.goodfon.ru/original/3360x1050/5/55/zakat-sunset-more-nebo.jpg' />
+            <div className='info'>
+                <img />
+                <div className='info__text'>
+                    <p></p>
+                    <p></p>
+                </div>
+                <div className='new-post'>
+                    <h3></h3>
+                </div>
+                <div className='posts'>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Profile
